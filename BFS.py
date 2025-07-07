c95c48b83a4a133686563c081ef95493872c4384
Kacper Kulka <kuleczkomen@gmail.com>
2025-07-07 11:23:54 +0000

    Updated BFS

---
+++ BFS.py
@@ -0,0 +1,22 @@
+from queue import deque
+INF = float('inf')
+
+def BFS(G,s):
+    V = len(G)
+    d = [INF] * V
+    d[s] = 0
+    q = deque()
+    q.append(s)
+    
+    while q:
+        u = q.popleft()
+        for v in G[u]:
+            if d[v] == INF:
+                d[v] = d[u] + 1
+                q.append(v)
+    
+    return d
+
+
+G = [[1,2,3], [], [], [4], []]
+print(BFS(G,0))
